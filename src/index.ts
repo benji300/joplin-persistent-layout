233879015df5c8938515ff509caa61e7a62fb3dd
benji300 <benji300@web.de>
2021-01-29 00:05:27 +0100

    Add command to persist current layout

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import joplin from 'api';
+import { MenuItemLocation, Path } from 'api/types';
 
 joplin.plugins.register({
   onStart: async function () {
@@ -32,17 +33,106 @@ joplin.plugins.register({
       { label: 'layout:viewer', panes: ["viewer"] } // Viewer
     ];
 
+    async function getAll(path: Path, query: any): Promise<any[]> {
+      query.page = 1;
+      let response = await DATA.get(path, query);
+      let result = !!response.items ? response.items : [];
+      while (!!response.has_more) {
+        query.page += 1;
+        let response = await DATA.get(path, query);
+        result.concat(response.items)
+      }
+      return result;
+    }
+
+    async function addTag(noteId: string, layout: LayoutType) {
+      let layoutTag = (await getAll(['tags'], { fields: ['id', 'title'], page: 1 }))
+        .find(x => x.title == LayoutSpec[layout].label);
+
+      if (!layoutTag) {
+        layoutTag = await DATA.post(['tags'], null, { title: LayoutSpec[layout].label });
+      }
+      await DATA.post(['tags', layoutTag.id, 'notes'], null, { id: noteId });
+    }
+
+    async function removeTag(noteId: string, layout: LayoutType) {
+      let layoutTag = (await getAll(['tags'], { fields: ['id', 'title'], page: 1 }))
+        .find(x => x.title == LayoutSpec[layout].label);
+
+      if (layoutTag) {
+        await DATA.delete(["tags", layoutTag.id, "notes", noteId]);
+      }
+    }
+
+    async function persistEditorLayout(noteId: string, layout: LayoutType) {
+      // first remove other tags
+      if (LayoutType.Editor != layout) {
+        await removeTag(noteId, LayoutType.Editor);
+      }
+      if (LayoutType.Split != layout) {
+        await removeTag(noteId, LayoutType.Split);
+      }
+      if (LayoutType.Viewer != layout) {
+        await removeTag(noteId, LayoutType.Viewer);
+      }
+      // add layout tag
+      await addTag(noteId, layout);
+    }
+
+    async function layoutMatchesVisiblePanes(layout: LayoutType): Promise<boolean> {
+      // noteVisiblePanes = ["editor","viewer"]
+      const noteVisiblePanes: any[] = await SETTINGS.globalValue('noteVisiblePanes');
+      return (LayoutSpec[layout].panes.sort().toString() == noteVisiblePanes.sort().toString());
+    }
+
     async function toggleVisiblePanes(layout: LayoutType) {
       for (let i: number = 0; i < 3; i++) {
-        // ["editor","viewer"]
-        let noteVisiblePanes: any[] = await SETTINGS.globalValue('noteVisiblePanes');
-
-        if (noteVisiblePanes.sort().toString() == LayoutSpec[layout].panes.sort().toString())
+        if (await layoutMatchesVisiblePanes(layout)) {
           break;
-
+        }
         await COMMANDS.execute('toggleVisiblePanes');
       }
     }
+
+    //#endregion
+
+    //#region COMMANDS
+
+    // Command: persistEditorLayout
+    // Desc: Persist the current editor layout for the selected note(s)
+    await COMMANDS.register({
+      name: 'persistEditorLayout',
+      label: 'Persist editor layout',
+      iconName: 'fas fa-columns',
+      enabledCondition: "someNotesSelected",
+      execute: async (noteIds: string[]) => {
+        // get selected note ids and return if empty
+        let selectedNoteIds = noteIds;
+        if (!selectedNoteIds) selectedNoteIds = await WORKSPACE.selectedNoteIds();
+        if (!selectedNoteIds) return;
+
+        const editor: boolean = await layoutMatchesVisiblePanes(LayoutType.Editor);
+        const split: boolean = await layoutMatchesVisiblePanes(LayoutType.Split);
+        const viewer: boolean = await layoutMatchesVisiblePanes(LayoutType.Viewer);
+
+        // persist for all selected notes
+        for (const noteId of selectedNoteIds) {
+          if (editor) {
+            await persistEditorLayout(noteId, LayoutType.Editor);
+          } else if (split) {
+            await persistEditorLayout(noteId, LayoutType.Split);
+          } else if (viewer) {
+            await persistEditorLayout(noteId, LayoutType.Viewer);
+          }
+        }
+      }
+    });
+
+    // add commands to menu
+    await joplin.views.menuItems.create('toolsPersist', 'persistEditorLayout', MenuItemLocation.Tools);
+
+    // add commands to notes context menu
+    await joplin.views.menuItems.create('notesContextPersist', 'persistEditorLayout', MenuItemLocation.NoteListContextMenu);
 
     //#endregion
 
@@ -54,26 +144,15 @@ joplin.plugins.register({
         const isCodeEditor: boolean = await SETTINGS.globalValue('editor.codeView');
 
         if (selectedNote && isCodeEditor) {
+          const noteTags = await getAll(['notes', selectedNote.id, 'tags'], { fields: ['id', 'title'], page: 1 });
+          // const tags: any = await DATA.get(['notes', selectedNote.id, 'tags'], { fields: ['title'] });
 
-          // retrieve tags from note
-          const tags: any = await DATA.get(['notes', selectedNote.id, 'tags'], { fields: ['title'] });
-
-          // editor
-          if (tags.items.find(x => x.title === LayoutSpec[LayoutType.Editor].label)) {
+          if (noteTags.find(x => x.title === LayoutSpec[LayoutType.Editor].label)) {        // editor
             await toggleVisiblePanes(LayoutType.Editor);
-            return;
-          }
-
-          // split view
-          if (tags.items.find(x => x.title === LayoutSpec[LayoutType.Split].label)) {
+          } else if (noteTags.find(x => x.title === LayoutSpec[LayoutType.Split].label)) {  // split view
             await toggleVisiblePanes(LayoutType.Split);
-            return;
-          }
-
-          // viewer
-          if (tags.items.find(x => x.title === LayoutSpec[LayoutType.Viewer].label)) {
+          } else if (noteTags.find(x => x.title === LayoutSpec[LayoutType.Viewer].label)) { // viewer
             await toggleVisiblePanes(LayoutType.Viewer);
-            return;
           }
         }
       } catch (error) {
